94b779f1f7804e4fb69545be1fe263979bbca81d
LuisKeys <luis.paradela@gmail.com>
2019-10-28 17:20:26 -0300

    Added WA time

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -28,32 +28,72 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.lblTime = new System.Windows.Forms.Label();
+      this.lblTimeWA = new System.Windows.Forms.Label();
+      this.lblWA = new System.Windows.Forms.Label();
+      this.lblTimeBA = new System.Windows.Forms.Label();
+      this.lblBA = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
-      // lblTime
+      // lblTimeWA
       // 
-      this.lblTime.AutoSize = true;
-      this.lblTime.Font = new System.Drawing.Font("Lucida Console", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.lblTime.ForeColor = System.Drawing.Color.GreenYellow;
-      this.lblTime.Location = new System.Drawing.Point(24, 21);
-      this.lblTime.Name = "lblTime";
-      this.lblTime.Size = new System.Drawing.Size(764, 144);
-      this.lblTime.TabIndex = 0;
-      this.lblTime.Text = "00:00:00";
+      this.lblTimeWA.AutoSize = true;
+      this.lblTimeWA.Font = new System.Drawing.Font("Lucida Console", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblTimeWA.ForeColor = System.Drawing.Color.GreenYellow;
+      this.lblTimeWA.Location = new System.Drawing.Point(214, 25);
+      this.lblTimeWA.Name = "lblTimeWA";
+      this.lblTimeWA.Size = new System.Drawing.Size(634, 120);
+      this.lblTimeWA.TabIndex = 0;
+      this.lblTimeWA.Text = "00:00:00";
+      // 
+      // lblWA
+      // 
+      this.lblWA.AutoSize = true;
+      this.lblWA.Font = new System.Drawing.Font("Lucida Console", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblWA.ForeColor = System.Drawing.Color.GreenYellow;
+      this.lblWA.Location = new System.Drawing.Point(2, 6);
+      this.lblWA.Name = "lblWA";
+      this.lblWA.Size = new System.Drawing.Size(236, 144);
+      this.lblWA.TabIndex = 1;
+      this.lblWA.Text = "WA";
+      // 
+      // lblTimeBA
+      // 
+      this.lblTimeBA.AutoSize = true;
+      this.lblTimeBA.Font = new System.Drawing.Font("Lucida Console", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblTimeBA.ForeColor = System.Drawing.Color.GreenYellow;
+      this.lblTimeBA.Location = new System.Drawing.Point(1301, 25);
+      this.lblTimeBA.Name = "lblTimeBA";
+      this.lblTimeBA.Size = new System.Drawing.Size(634, 120);
+      this.lblTimeBA.TabIndex = 2;
+      this.lblTimeBA.Text = "00:00:00";
+      // 
+      // lblBA
+      // 
+      this.lblBA.AutoSize = true;
+      this.lblBA.Font = new System.Drawing.Font("Lucida Console", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblBA.ForeColor = System.Drawing.Color.GreenYellow;
+      this.lblBA.Location = new System.Drawing.Point(1085, 6);
+      this.lblBA.Name = "lblBA";
+      this.lblBA.Size = new System.Drawing.Size(236, 144);
+      this.lblBA.TabIndex = 3;
+      this.lblBA.Text = "BA";
       // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-      this.ClientSize = new System.Drawing.Size(800, 181);
-      this.Controls.Add(this.lblTime);
+      this.ClientSize = new System.Drawing.Size(2040, 147);
+      this.Controls.Add(this.lblBA);
+      this.Controls.Add(this.lblTimeBA);
+      this.Controls.Add(this.lblWA);
+      this.Controls.Add(this.lblTimeWA);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
       this.Name = "MainForm";
       this.Opacity = 0.8D;
       this.Text = "MainForm";
       this.Load += new System.EventHandler(this.MainForm_Load);
+      this.MouseEnter += new System.EventHandler(this.MainForm_MouseEnter);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -61,7 +101,10 @@
 
     #endregion
 
-    private System.Windows.Forms.Label lblTime;
+    private System.Windows.Forms.Label lblTimeWA;
+    private System.Windows.Forms.Label lblWA;
+    private System.Windows.Forms.Label lblTimeBA;
+    private System.Windows.Forms.Label lblBA;
   }
 }
 
--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -13,7 +13,8 @@ namespace Clock
   public partial class MainForm : Form
   {
 
-    System.Windows.Forms.Timer _timer = null;
+    System.Windows.Forms.Timer _timeTimer = null;
+    System.Windows.Forms.Timer _viewTimer = null;
 
     public MainForm()
     {
@@ -25,22 +26,33 @@ namespace Clock
       refreshTime();
       this.TopMost = true;
       Rectangle workingArea = Screen.PrimaryScreen.WorkingArea;
-        // use 'Screen.AllScreens[1].WorkingArea' for secondary screen
-        this.Left = workingArea.Left + workingArea.Width - this.Size.Width;
-        this.Top = workingArea.Top + workingArea.Height - this.Size.Height;
-        if (_timer == null) {
-          _timer = new System.Windows.Forms.Timer();
-        _timer.Tick += _timer_Tick;
-        _timer.Interval = 1000;
-        _timer.Enabled = true;
+      // use 'Screen.AllScreens[1].WorkingArea' for secondary screen
+      this.Left = workingArea.Left + workingArea.Width - this.Size.Width;
+      this.Top = workingArea.Top + workingArea.Height - this.Size.Height;
+      
+      if (_viewTimer == null) {
+        _viewTimer = new System.Windows.Forms.Timer();
+        _viewTimer.Tick += _viewTimer_Tick;
+        _viewTimer.Interval = 5000;
+        _viewTimer.Enabled = true;
+      }
+
+      if (_timeTimer == null) {
+          _timeTimer = new System.Windows.Forms.Timer();
+        _timeTimer.Tick += _timeTimer_Tick;
+        _timeTimer.Interval = 1000;
+        _timeTimer.Enabled = true;
         }
     }
 
-    private void _timer_Tick(object sender, EventArgs e)
-    {
-      _timer.Stop();
+    private void _timeTimer_Tick(object sender, EventArgs e) {
+      _timeTimer.Stop();
       refreshTime();
-      _timer.Start();
+      _timeTimer.Start();
+    }
+
+    private void _viewTimer_Tick(object sender, EventArgs e) {
+      this.Visible = true;
     }
 
     private void refreshTime() {
@@ -48,7 +60,19 @@ namespace Clock
       int min = DateTime.Now.Minute;
       int sec = DateTime.Now.Second;
       string time = hour.ToString().PadLeft(2, '0') + ":" + min.ToString().PadLeft(2, '0') + ":" + sec.ToString().PadLeft(2, '0');
-      lblTime.Text = time;
+      lblTimeBA.Text = time;
+
+      hour -= 4;
+      if (hour < 0)
+        hour += 12;
+
+      time = hour.ToString().PadLeft(2, '0') + ":" + min.ToString().PadLeft(2, '0') + ":" + sec.ToString().PadLeft(2, '0');
+      lblTimeWA.Text = time;
+    }
+
+    private void MainForm_MouseEnter(object sender, EventArgs e)
+    {
+      this.Visible = false;
     }
   }
 }
